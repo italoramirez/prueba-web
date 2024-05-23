a58577dffe716c3cbd1ca227fe3e8aa5ac9dfa51
Italo Ramirez P <italoramirezp@gmail.com>
2024-05-23 02:36:52 -0500

    Crea frontend

---
+++ src/components/private/global/BaseButton.vue
@@ -0,0 +1,19 @@
+<template>
+  <button
+      class="bg-black hover:shadow-lg hover:text-cyan-350 font-semibold text-white rounded-lg px-4 transition-all h-10"
+      :disabled="disabled"
+      @click="emit('click')">{{text}}<slot></slot>
+  </button>
+</template>
+<script setup lang="ts">
+defineProps<{
+  disabled?: {
+    type: Boolean,
+    default: false,
+  },
+  text: String,
+}>()
+const emit = defineEmits<{
+  (event: 'click'): void,
+}>()
+</script>
